301268e4b67740501dda8e01a42afa64b7f1300c
christopherhaase <christopherhaa@gmail.com>
2022-03-21 14:35:15 +0100

    connection made

--- src/App.js
+++ src/App.js
@@ -1,22 +1,37 @@
-import logo from './logo.svg';
+import {useState, useEffect} from 'react'
 import './App.css';
+import axios from "axios";
+
+
+
+
+
+const fetchData = () => {
+
+axios.get('http://localhost:8080/')
+
+.then(result => {
+    console.log(result);    
+})
+
+.catch(error => {
+    console.log(error);
+})
+}
 
 function App() {
+
+  useEffect(() => {
+    fetchData()
+  })
+
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+      
+        
       </header>
     </div>
   );
