34fc3df004b1e9021a0a059cd9de35cd14256c7a
danielhogstrom <daniel.hogstrom@gmail.com>
2022-03-22 16:34:51 +0100

    minor changes

--- src/App.js
+++ src/App.js
@@ -1,16 +1,9 @@
-import { useState, useEffect } from "react";
 import "./App.css";
-import Header from "./components/Header/Header";
-import Content from "./components/Content/Content";
-import Footer from "./components/Footer/Footer";
 import Login from "./components/Login/Login";
 
 function App() {
   return (
     <div className="App">
-      <Header />
-      <Content isLoggedIn={true} />
-      <Footer />
       <Login />
     </div>
   );
