6e14f22f94ae10544fcd43e68090d4d05a6ed2cc
danielhogstrom <daniel.hogstrom@gmail.com>
2022-03-22 10:56:14 +0100

    add style

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,14 +1,12 @@
 import "./header.css";
-import homebutton from "./icons/home-button.png";
+/* import homebutton from "./icons/home-button.png"; */
 import uploadbutton from "./icons/upload-button.png";
 
 export default function Header() {
   return (
     <div className="header">
       <div className="logo">MEMESTAGRAM</div>
-      <div>
-        <img src={homebutton} />
-      </div>
+      <div>{/*    <img src={homebutton} /> */}</div>
       <div>
         <img src={uploadbutton} />
       </div>
