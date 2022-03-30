9fdb6d29c6fd1e7d9539e2e41559de5ba2c19e9a
danielhogstrom <daniel.hogstrom@gmail.com>
2022-03-30 15:52:25 +0200

    styling

--- src/components/MyPage/MyPage.js
+++ src/components/MyPage/MyPage.js
@@ -7,17 +7,10 @@ import { useState } from "react";
 import Button from "@mui/material/Button";
 import { red } from "@mui/material/colors";
 import Avatar from "@mui/material/Avatar";
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-  Link
-} from "react-router-dom";
-import { useLocation } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { useLocation } from "react-router-dom";
 import { IconButton, Switch } from "@mui/material";
 import "./mypage.css";
-
-
 
 export default function MyPage(props) {
   const [user, setUser] = useState({});
@@ -58,6 +51,44 @@ export default function MyPage(props) {
         >
           <Button onClick={handleClick}>Go back</Button>
         </Grid>
+        <Grid item sm={12}>
+          <Header />
+
+          <div
+            className="avatarwindow"
+            style={{
+              display: "flex",
+            }}
+          >
+            <div>
+              <Avatar className="avatarpic">
+                <img
+                  src="https://images.freeimages.com/images/large-previews/7f0/old-man-1561812.jpg"
+                  className="actualpic"
+                ></img>
+              </Avatar>
+            </div>
+
+            <span className="username">{data.user.username}</span>
+
+            <span className="bio">
+              Sed ut perspiciatis unde omnis iste natus error sit voluptatem
+              accusantium doloremque laudantium, totam rem aperiam.
+              {data.user.bio}
+            </span>
+          </div>
+          <div className="following">Following: 9</div>
+        </Grid>
+        <Grid
+          item
+          sm={12}
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            marginTop: "10px",
+          }}
+        ></Grid>
         {data.memes
           .slice(0)
           .filter((meme) => meme.creator.id == data.user.id)
@@ -75,75 +106,7 @@ export default function MyPage(props) {
                   height: "100%",
                 }}
               >
-                <Grid item sm={12}>
-                  <Header />
-
-
-
-                  <div className="avatarwindow"
-                  style={{
-                  display: "flex",
-                  }}>
-
-                    <div>
-                    <Avatar className="avatarpic">
-                      <img src="https://images.freeimages.com/images/large-previews/7f0/old-man-1561812.jpg" className="actualpic"></img>
-                    </Avatar>
-                    </div>
-
-                    <span className="username">
-                      {data.user.username}
-                    </span>
-
-                    <span className="bio">
-                    Sed ut perspiciatis unde omnis iste natus error sit voluptatem 
-                    accusantium doloremque laudantium, totam rem aperiam.
-                      {data.user.bio}
-                    </span>    
-                  </div>
-                  <div className="following">
-                      Following: 9
-                  </div>
-
-
-                </Grid>
-                <Grid
-                  item
-                  sm={12}
-                  style={{
-                    display: "flex",
-                    justifyContent: "center",
-                    alignItems: "center",
-                    marginTop: "10px",
-                  }}
-                >                
-                 </Grid>
-                {data.memes
-                  .slice(0)
-                  .filter(meme => meme.creator.id==data.user.id)
-                  .map((meme, index) => {
-                    return (
-                      <Grid
-                        key={index}
-                        item
-                        sm={6}
-                        md={4}
-                        style={{
-                          display: "flex",
-                          justifyContent: "center",
-                          width: "100%",
-                          height: "100%",
-                        }}
-                      >
-                        <MemeCard meme={meme} user={user} />
-                      </Grid>
-                    );
-                  })}
-                <Grid
-                  item
-                  sm={12}
-                  style={{ display: "flex", justifyContent: "center" }}
-                ></Grid>
+                <MemeCard meme={meme} user={user} />
               </Grid>
             );
           })}
@@ -153,6 +116,12 @@ export default function MyPage(props) {
           style={{ display: "flex", justifyContent: "center" }}
         ></Grid>
       </Grid>
+
+      <Grid
+        item
+        sm={12}
+        style={{ display: "flex", justifyContent: "center" }}
+      ></Grid>
     </Box>
   );
 }
